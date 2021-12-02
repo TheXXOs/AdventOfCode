875850edc08edbe8a90aa36edbb44275b027b84e
TheXXOs <44630816+TheXXOs@users.noreply.github.com>
2021-12-02 16:39:28 +1030

    Day copy
    This is how I'll slightly streamline the process; have file-opening code already written!

---
+++ My Solutions/2021/Day copy/1.py
@@ -0,0 +1,9 @@
+# begin opening code
+with open("in.txt") as f:
+    inp = f.read().split('\n')
+# end opening code
+
+# put daily code below here, copy this file for part 2
+
+
+# put daily code above here
