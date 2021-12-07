b8b904c74a57ad75b0b846add79802d12fddefb1
TheXXOs <44630816+TheXXOs@users.noreply.github.com>
2021-12-07 16:00:33 +1030

    Update "Day copy\1.py"
    so that there's less comments and there's an optional way to convert the list of strings to another format

--- My Solutions/2021/Day copy/1.py
+++ My Solutions/2021/Day copy/1.py
@@ -1,9 +1,4 @@
-# begin opening code
+# copy this file for part 2
 with open("in.txt") as f:
     inp = f.read().split('\n')
-# end opening code
-
-# put daily code below here, copy this file for part 2
-
-
-# put daily code above here
+# inp = [int(i) for i in inp]
